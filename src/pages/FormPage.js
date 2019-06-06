21bc971ea95ce9199b2256f8da322614a130f239
Kasidit Phoncharoen <kasidit@inox.co.th>
2019-06-06 23:36:51 +0700

    Blind data in Form page

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -5,18 +5,20 @@ import '../css/FormPage.css';
 export default class FormPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { rows: [[]] };
+        this.state = { rows: props.data };
+        console.log(props)
     }
 
     render() {
-        return (
-          <div>
-            <button onClick={ () => {window.print()} }>Print</button>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} id="hello">
-              <RequestForm />
-              {/* <RequestForm /> */}
-            </div>
+      const { rows } = this.state
+      return (
+        <div>
+          <button onClick={ () => {window.print()} }>Print</button>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', }} id="hello">
+            <RequestForm data={rows}/>
+            <RequestForm data={rows}/>
           </div>
-        )
+        </div>
+      )
     }
 }
