c7e7cbf179c74d7c3796c47791604ddfaf7c9373
wisatong <wisa.pow@gmail.com>
2019-06-06 20:01:12 +0700

    get credentials from .env

--- src/components/C.js
+++ src/components/C.js
@@ -7,8 +7,8 @@ export default class C extends React.Component {
     }
 
     componentDidMount() {
-        const API_KEY = 'AIzaSyANBucM2vlWnqhijUGLD-9SurgT3LdAjvw';
-        const SHEET_ID = '1sPET20Cb6NUIsBX7WUjXxEAaEqUONz1gHyk_5CLmNOc';
+        const API_KEY = process.env.API_KEY;
+        const SHEET_ID = process.env.SHEET_ID;
 
         fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Sheet1!A3:K?key=${API_KEY}`)
             .then(res => res.json())
