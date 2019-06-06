5cb8e4c7d560668544bbf2a030e4c83326fdb043
Kasidit Phoncharoen <kasidit@inox.co.th>
2019-06-06 23:46:57 +0700

    Add more function

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -21,12 +21,24 @@ export default class FormPage extends React.Component {
       });
     }
 
+    downloadHighPDF(quality = 1) {
+      const filename  = 'VISTEC_Request_Form.pdf';
+  
+      html2canvas(document.querySelector('#hello'), 
+                  {scale: quality}
+               ).then(canvas => {
+        let pdf = new jsPDF('p', 'mm', 'a4');
+        pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+        pdf.save(filename);
+      });
+    }
+
     render() {
       const { rows } = this.state
       return (
         <div>
           <button onClick={ () => {window.print()} }>Print</button>
-          <button onClick={ () => { this.downloadPDF() } }>PDF</button>
+          <button onClick={ () => { this.downloadHighPDF() } }>PDF</button>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', }} id="hello">
             <RequestForm data={rows}/>
             <RequestForm data={rows}/>
