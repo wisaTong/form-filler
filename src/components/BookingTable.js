583f71e3441daf566e7bb7298396bc60928431bc
wisatong <wisa.pow@gmail.com>
2019-06-06 21:03:31 +0700

    C -> BookingTable

--- src/components/BookingTable.js
+++ src/components/BookingTable.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export default class C extends React.Component {
+export default class BookingTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = { rows: [[]] };
     }
 
     componentDidMount() {
-        const API_KEY = process.env.API_KEY;
-        const SHEET_ID = process.env.SHEET_ID;
+        const API_KEY = process.env.REACT_APP_API_KEY;
+        const SHEET_ID = process.env.REACT_APP_SHEET_ID;
 
         fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Sheet1!A3:K?key=${API_KEY}`)
             .then(res => res.json())
@@ -16,6 +16,8 @@ export default class C extends React.Component {
     }
 
     render() {
+        console.log(this.state.rows);
+        
         return <p>{this.state.rows[0][0]}</p>
     }
 }
