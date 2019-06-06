953c90cb251a579948fb70294e4873e8501d6b18
Kasidit Phoncharoen <kasidit@inox.co.th>
2019-06-06 23:44:51 +0700

    Add download PDF

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import RequestForm from '../components/RequestForm'
 import '../css/FormPage.css';
+import html2canvas from 'html2canvas'
+import jsPDF from 'jspdf'
 
 export default class FormPage extends React.Component {
     constructor(props) {
@@ -9,11 +11,22 @@ export default class FormPage extends React.Component {
         console.log(props)
     }
 
+    downloadPDF() {
+      const filename  = 'VISTEC_Request_Form.pdf';
+  
+      html2canvas(document.querySelector('#hello')).then(canvas => {
+        let pdf = new jsPDF('p', 'mm', 'a4');
+        pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+        pdf.save(filename);
+      });
+    }
+
     render() {
       const { rows } = this.state
       return (
         <div>
           <button onClick={ () => {window.print()} }>Print</button>
+          <button onClick={ () => { this.downloadPDF() } }>PDF</button>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', }} id="hello">
             <RequestForm data={rows}/>
             <RequestForm data={rows}/>
