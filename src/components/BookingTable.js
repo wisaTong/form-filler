a1c8c856ba23b8cdf6600690f429f4aa3f184c5b
wisatong <wisa.pow@gmail.com>
2019-06-07 17:53:10 +0700

    more tweaks

--- src/components/BookingTable.js
+++ src/components/BookingTable.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FormPage from '../pages/FormPage'
 import '../css/BookingTable.css'
+import logo from '../assets/vistec-logo.svg'
 
 export default class BookingTable extends React.Component {
   constructor(props) {
@@ -17,6 +18,15 @@ export default class BookingTable extends React.Component {
       .then(res => res.json())
       .then(json => this.setState({ rows: json['values'].sort(), isLoaded: true }));
   }
+  
+  ColorDesc(props) {
+    return (
+      <div style={{ display: 'inline-block', margin: '3rem 2rem 1rem 2rem' }}>
+        <div style={{ height: '15px', width: '40px', backgroundColor: props.color, borderRadius: '10px', display: 'inline-block' }}></div>
+        <p style={{ display: 'inline', margin: '0 1rem' }}>{props.desc}</p>
+      </div>
+    );
+  }
 
   TableRow(props) {
     const row = props.row;
@@ -31,15 +41,15 @@ export default class BookingTable extends React.Component {
       <div class={"row-card " + statusStyle(status)}>
         <div class="slot passenger">
           <h3>Passenger</h3>
-          <p>Name: {row[0]}</p>
-          <p>Phone number: {row[1]}</p>
+          <p>Name: <em>{row[0]}</em></p>
+          <p>Phone number: <em>{row[1]}</em></p>
         </div>
         <div class="slot trip">
           <div class="pickup">
             <h3>Pickup</h3>
-            <p>Date: {row[2]}</p>
-            <p>Time: {row[3]}</p>
-            <p>Location: {row[4]}</p>
+            <p>Date: <em>{row[2]}</em></p>
+            <p>Time: <em>{row[3]}</em></p>
+            <p>Location: <em>{row[4]}</em></p>
           </div>
           <div class="drop-off">
             <h3>Drop-off</h3>
@@ -54,7 +64,7 @@ export default class BookingTable extends React.Component {
           <p>Phone number: {row[9]}</p>
         </div>
         <button onClick={() => ReactDOM.render(<FormPage data={row}/>, document.getElementById('root'))}>
-          Print
+          Print form
         </button>
       </div>
     )
@@ -63,6 +73,11 @@ export default class BookingTable extends React.Component {
   render() {
     return this.state.isLoaded ? (
       <div class="container">
+        <img src={logo} alt="Logo" style={{ width: '25rem', marginBottom: '1rem' }} />
+        <this.ColorDesc color="#B90038" desc="N/A" />
+        <this.ColorDesc color="#5c267E" desc="Printed" />
+        <this.ColorDesc color="#FFA642" desc="Requested" />
+        <this.ColorDesc color="#00CD8A" desc="Confirmed" />
         {this.state.rows.map(row => (
           <this.TableRow row={row} />
         ))}
